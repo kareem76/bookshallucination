cfa66e25b306eb13fa1a46b511ba670d66dd2912
kareem76 <kareem1976@live.com>
2025-02-02 21:32:29 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -63,7 +63,7 @@ class BookScraper
         title = book_page.at('div.p-title')&.text&.strip
         author = book_page.at('div.p-author')&.text&.strip&.gsub(/^لـ /, '')
         image_url = book_page.at('.p-cover img')&.[]('src')
-        year = book_page.at('.p-info b:contains("تاريخ النشر")')&.text&.split(':')&.last&.strip
+        year = year = book_page.at('.p-info b:contains("تاريخ النشر")')&.next&.text&.strip
         publisher = book_page.at('.p-info b:contains("الناشر")')&.next&.text&.strip
         d_content = book_page.at('span.desc.nabza d')
 
