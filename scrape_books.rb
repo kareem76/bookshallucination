a99fcb1101fb81958f27a8178342570ed75d48e3
kareem76 <kareem1976@live.com>
2025-07-04 15:48:15 +0000

    zira3-3loum

--- scrape_books.rb
+++ scrape_books.rb
@@ -8,16 +8,20 @@ require 'fileutils'
 require 'parallel'
 
 # Capybara Configuration
-Capybara.register_driver :selenium_headless do |app|
-  options = Selenium::WebDriver::Firefox::Options.new
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--headless')
   options.add_argument('--disable-gpu')
   options.add_argument('--window-size=1920,1080')
-  options.add_argument('--no-sandbox')  # for GitHub Actions
-  Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
+Capybara.default_driver = :selenium_chrome_headless
 Capybara.default_max_wait_time = 10
+
 
 class BookScraper
   include Capybara::DSL
